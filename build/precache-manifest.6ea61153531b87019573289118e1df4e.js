98148b16720d6eb5be07121a4f930aa6f57418c5
JasonCodez <jason13garrett@gmail.com>
2022-06-18 22:10:48 -0500

    jobly frontend react

---
+++ build/precache-manifest.6ea61153531b87019573289118e1df4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc510f8bdfbb4b0ed56b3a09be84d2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a815f78c1be53ea6e276",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "016d5f707f1af58392d9",
+    "url": "/static/css/main.658eb3e8.chunk.css"
+  },
+  {
+    "revision": "a815f78c1be53ea6e276",
+    "url": "/static/js/2.785c4bba.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.785c4bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016d5f707f1af58392d9",
+    "url": "/static/js/main.61bfa715.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.61bfa715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b8b38e359d53208b9e",
+    "url": "/static/js/runtime-main.8a240d35.js"
+  }
+]);
